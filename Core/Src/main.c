91bfeab622f67597c422f536b23ba682f729101b
George Vigelette <george@openwater.cc>
2025-10-09 10:23:40 -0400

    update version

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -92,7 +92,7 @@ DMA_HandleTypeDef hdma_usart3_tx;
 
 /* USER CODE BEGIN PV */
 
-uint8_t FIRMWARE_VERSION_DATA[3] = {2, 0, 0};
+uint8_t FIRMWARE_VERSION_DATA[3] = {2, 0, 1};
 uint32_t id_words[3] = {0};
 
 // Define the pointers
